8e9425e6d86c48ff0bb21466c86728f5d930d74f
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2021-07-04 12:50:59 +0100

    Initial Commit

---
+++ List Problems/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  List Problems
+//
+//  Created by Mohammed Alqumairi on 04/07/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
